4cd4d2d054e6af82f683781a4d34297418e63c23
Maverick-Griot <griot@maverickgriot.com>
2024-12-30 17:25:11 -0500

    Adding player object to main along with helper methods

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import *
+import player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
 
+    gameClock = pygame.time.Clock()
+    playerShip = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
     running = True
     while running:
         for event in pygame.event.get():
@@ -13,7 +17,9 @@ def main():
                 running = False
 
         screen.fill(BLACK)
+        playerShip.draw(screen)
         pygame.display.flip()
+        dt = gameClock.tick(60)/1000
 
     pygame.quit()
 
