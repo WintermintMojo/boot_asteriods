d4d61af2a156580db246e7e91902dc9ef9b01784
Maverick-Griot <griot@maverickgriot.com>
2024-12-31 13:27:10 -0500

    Adding player foward and backward movement

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.thrusting = False
-    # in the player class
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -20,8 +20,24 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, WHITE, self.triangle(),2)
 
     def update(self, dt):
+        self.thrusting = False
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
-           self.rotation -= PLAYER_TURN_SPEED * dt
-        if keys[pygame.K_d]:
-            self.rotation += PLAYER_TURN_SPEED * dt
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.rotate(-dt)
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.rotate(dt)
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.move(dt)
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            pass
+        while keys[pygame.K_LSHIFT]:
+            self.thrusting = True
+
+    def rotate(self,dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def move(self,dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
